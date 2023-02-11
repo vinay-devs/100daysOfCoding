2384ad1d4a600937075b3028532393f217e6bc07
Vinay Dev S <69599514+vinay-devs@users.noreply.github.com>
2023-02-11 23:39:50 +0530

    Day51

---
+++ Day51/main.js
@@ -0,0 +1,10 @@
+const container = document.querySelector(".container");
+const body = document.querySelector("body");
+
+body.addEventListener("mousemove", function (e) {
+	let x = e.clientX/5 ;
+	let y = e.clientY/5;
+	
+	container.style.backgroundPositionX = `${x}px`;
+	container.style.backgroundPositionY = `${y}px`;
+})

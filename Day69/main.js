1c2857d62b22969745123e2b907cbf63cfee2fa8
Vinay Dev S <vinaydevs@gmail.com>
2023-03-01 21:52:28 +0530

    Day70

--- Day69/main.js
+++ Day69/main.js
@@ -1,14 +1,17 @@
 let containerImage = document.querySelector(".images");
-
-const url = "https://source.unsplash.com/random/"
+const searchTerm = ["city", "car", "bike", "apple", "bmw", "old", "school", "candlelight", "Amazon"];
 
 // const getPhotos = async(url) => {
-//     const data = await fetch(url).then((res) => { res.json() })
-//     return data;
+//     const data = await fetch(url)
+//     return data.url;
 // }
 
 
 for (let i = 0; i < 9; i++) {
+    const url = `https://source.unsplash.com/random/?${searchTerm[i]}`
+        // const data = getPhotos(url);
+        // console.log(data);
+
     let img = document.createElement('img');
     img.setAttribute('src', url);
     containerImage.appendChild(img);
